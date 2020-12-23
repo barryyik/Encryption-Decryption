7aabefd2a23c164131812d44b75724d0f729dbeb
barryyik <66619438+barryyik@users.noreply.github.com>
2020-12-23 18:56:26 +0800

    add changes to enhance encryption security

--- sketch.js
+++ sketch.js
@@ -15,11 +15,13 @@ let decryptedAESMessage;
 
 function setup() {
 	
+	
 	canvas = createCanvas(windowWidth, windowHeight);
 	ranNum1 = random(255);
 	ranNum2 = random(255);
 	ranNum3 = random(255);
 	
+	//input1 = createInput();
 	input1 = createElement('textarea', 'your text here');
 	input1.position(10,10);
 	input1.size(400, 100);
@@ -47,15 +49,56 @@ function setup() {
 }
 
 function draw() {
+	// background(ranNum1, ranNum2, ranNum3);
 	background(134, 216, 174);
 }
 
 function encryptText(){
-	encryptedAESMessage = CryptoJS.AES.encrypt(input1.value(), input2.value());
+	//var tempText = input1.value();
+	
+	encryptedAESMessage = encryptData(input1.value(), input2.value());
 	document.getElementById('outputtext1').value = encryptedAESMessage;
+
 }
 
 function decryptText(){
-	decryptedAESMessage = CryptoJS.AES.decrypt(input1.value(), input2.value());
-	document.getElementById('outputtext1').value = decryptedAESMessage.toString(CryptoJS.enc.Utf8);
+
+	decryptedAESMessage = decryptData(input1.value(), input2.value());
+	document.getElementById('outputtext1').value = decryptedAESMessage;
+
+}
+
+function encryptData(msg, pwd) {
+    var salt = CryptoJS.lib.WordArray.random(128/8);
+    var key = CryptoJS.PBKDF2(pwd, salt, {
+        keySize: 512/32,
+        iterations: 500
+    });
+    var iv = CryptoJS.lib.WordArray.random(128/8);
+    var encrypted = CryptoJS.AES.encrypt(msg, key, { 
+        iv: iv, 
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+
+    var encryptedMessage = salt.toString()+ iv.toString() + encrypted.toString();
+    return encryptedMessage;
+}
+
+function decryptData(msg, pwd) {
+    var salt = CryptoJS.enc.Hex.parse(msg.substr(0, 32));
+    var iv = CryptoJS.enc.Hex.parse(msg.substr(32, 32))
+    var encrypted = msg.substring(64);
+
+    var key = CryptoJS.PBKDF2(pwd, salt, {
+        keySize: 512/32,
+        iterations: 500
+    });
+
+    var decrypted = CryptoJS.AES.decrypt(encrypted, key, { 
+        iv: iv, 
+        padding: CryptoJS.pad.Pkcs7,
+        mode: CryptoJS.mode.CBC
+    });
+    return decrypted.toString(CryptoJS.enc.Utf8);
 }
